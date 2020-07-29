adeb385de52de7e0b8f2a00bbddd2cd7a0269a9f
Eric Bottard <ebottard@pivotal.io>
2020-07-29 16:46:39 +0200

    Fix flaky test due to early GC of metrics

--- src/main/java/reactor/netty/resources/PooledConnectionProvider.java
+++ src/main/java/reactor/netty/resources/PooledConnectionProvider.java
@@ -27,6 +27,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Queue;
+import java.util.WeakHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.function.BiPredicate;
 import java.util.function.Function;
@@ -78,6 +79,17 @@ final class PooledConnectionProvider implements ConnectionProvider {
 
 	final ConcurrentMap<PoolKey, InstrumentedPool<PooledConnection>> channelPools =
 			PlatformDependent.newConcurrentHashMap();
+	/**
+	 * This map keeps a weakref to the {@link InstrumentedPool#metrics() metrics} of created pools through the same PoolKey that is used in
+	 * {@link #channelPools}. This is so that metrics providing objects don't get garbage collected too early,
+	 * as metrics-related frameworks (such as micrometer) don't hold strong references to metrics providing objects
+	 * (rightfully so). When the PoolKey is garbage collected, the metrics will become garbage collectable again.
+	 *
+	 * @see #acquire(Bootstrap)
+	 * @see #disposeLater()
+	 */
+	private final Map<PoolKey, ConnectionPoolMetrics> poolMetrics = new WeakHashMap<>();
+
 	final String                          name;
 	final Map<SocketAddress, PoolFactory> poolFactoryPerRemoteHost = new HashMap<>();
 	final PoolFactory                     defaultPoolFactory;
@@ -151,14 +163,18 @@ final class PooledConnectionProvider implements ConnectionProvider {
 						new PooledConnectionAllocator(bootstrap, poolFactory, opsFactory).pool;
 
 				if (poolFactory.metricsEnabled || BootstrapHandlers.findMetricsSupport(bootstrap) != null) {
+					// registrar is null when metrics are enabled on HttpClient level or
+					// with the `metrics(boolean metricsEnabled)` method on ConnectionProvider
 					MeterRegistrar registrar = poolFactory.registrar != null ?
-							// registrar is null when metrics are enabled on HttpClient level
 							poolFactory.registrar.get() : MicrometerPooledConnectionProviderMeterRegistrar.INSTANCE;
 
+					ConnectionPoolMetrics.DelegatingConnectionPoolMetrics metrics =
+							new ConnectionPoolMetrics.DelegatingConnectionPoolMetrics(newPool.metrics());
+					poolMetrics.put(poolKey, metrics);
 					registrar.registerMetrics(name,
 							poolKey.hashCode() + "",
 							Metrics.formatSocketAddress(remoteAddress),
-							new ConnectionPoolMetrics.DelegatingConnectionPoolMetrics(newPool.metrics()));
+							metrics);
 				}
 				return newPool;
 			});
